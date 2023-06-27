8aa2232d1fae07f2efab39e841a8fd3f0aa248e4
DieSchoeneWolke <jan.schoenfeld@tutanota.de>
2023-06-27 12:17:31 +0200

    program that switches the values after input

---
+++ Day1/ex4.py
@@ -0,0 +1,23 @@
+# program that switches the values after input stored in the variables a and b when you hit run
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+c = a
+a = b
+b = c
+
+# add another variable
+# think of swapping 2 drinks in wrong glasses
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
