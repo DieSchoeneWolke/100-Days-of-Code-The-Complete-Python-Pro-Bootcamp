80089570b2833c5db56c2c5595617e368777d6e8
DieSchoeneWolke <jan.schoenfeld@tutanota.de>
2023-06-27 11:47:20 +0200

    added goals as comments

--- Day1/ex1.py
+++ Day1/ex1.py
@@ -1,3 +1,8 @@
+#match the example output shown below exactly
+#Day 1 - Python Print Function
+#The function is declared like this:
+#print('what to print')
+
 print ('Day 1 - Python Print Function')
 print ("The function is declared like this:")
 print("print('what to print')")
--- Day1/ex3.py
+++ Day1/ex3.py
@@ -1,2 +1,3 @@
+#program that asks the user for an input and counts the characters of the string
 user = input("What is your name?")
 print(len(user))
