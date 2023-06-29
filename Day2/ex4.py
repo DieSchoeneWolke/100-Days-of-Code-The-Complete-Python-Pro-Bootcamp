3a5c0244e9b0d4a683bae3bc2a7e39c1e2aeac72
DieSchoeneWolke <jan.schoenfeld@tutanota.de>
2023-06-29 16:50:10 +0200

    converted datatypes

--- Day2/ex4.py
+++ Day2/ex4.py
@@ -11,7 +11,7 @@
 #convert datatypes
 
 print("Welcome to the tip calculator.")
-bill = print(input("What was the total bill?"))
-percentage = print(input("What percentage tip would you like to give?" + " 10, 12 or 15?"))
-split = print(input("How many people to split the bill?"))
-result = print(f"Each person should pay:") 
+bill = float(input("What was the total bill? $"))
+percentage = int(input("What percentage tip would you like to give?" + " 10, 12 or 15?"))
+split = int(input("How many people to split the bill?"))
+result = print(f"Each person should pay:")
