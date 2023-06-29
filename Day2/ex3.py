307cef74cf1e1a1513c00f7c829c4de07dc7e5bb
DieSchoeneWolke <jan.schoenfeld@tutanota.de>
2023-06-29 13:27:02 +0200

    pseudocode

--- Day2/ex3.py
+++ Day2/ex3.py
@@ -7,9 +7,13 @@ age = input("What is your current age? ")
 #convert
 #calc
 #print f string
-convert1 = int(age)
+# There are 365 days in a year, 52 weeks in a year and 12 months in a year.
 
+convert1 = int(age)
+calc1 = convert1 /
 days = 
 weeks = 
 months =
 print(f"You have {days}, {weeks}, {months} left.")
+
+
