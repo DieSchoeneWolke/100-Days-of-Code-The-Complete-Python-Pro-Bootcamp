793cf7914ca096d68259070659e400f4f451da30
DieSchoeneWolke <jan.schoenfeld@tutanota.de>
2023-06-29 12:04:42 +0200

    added subscripting

--- Day2/ex1.py
+++ Day2/ex1.py
@@ -8,3 +8,8 @@ two_digit_number = input("Type a two digit number: ")
 #two_digit_number subscripting 
 #convert str to int
 #calc
+
+number1 = two_digit_number[0]
+number2 = two_digit_number[1]
+
+print(number1 + "\n" + number2)
