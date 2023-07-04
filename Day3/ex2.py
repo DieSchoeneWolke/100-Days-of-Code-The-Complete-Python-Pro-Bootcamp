d9b5b3860b1df2ea0a8b89f2763a1f7ee4f57dc3
DieSchoeneWolke <jan.schoenfeld@tutanota.de>
2023-07-04 11:50:14 +0200

    elif 3

--- Day3/ex2.py
+++ Day3/ex2.py
@@ -18,3 +18,5 @@ elif calc < 25:
   print(f"Your BMI is {calc}, you have a normal weight.")
 elif calc < 30:
   print(f"Your BMI is {calc}, you are slightly overweight.")
+elif calc < 35:
+  print(f"Your BMI is {calc}, you are obese.")
