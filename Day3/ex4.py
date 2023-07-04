cee80abba1641ea6b380b908aa9933a5e87a42a9
DieSchoeneWolke <jan.schoenfeld@tutanota.de>
2023-07-04 12:23:12 +0200

    if 2 broken need fix

--- Day3/ex4.py
+++ Day3/ex4.py
@@ -9,6 +9,7 @@ year = int(input("Which year do you want to check? "))
 # else
 
 if year % 4 == 0:
-    print("Leap year.")
+    if year % 100 == 0:
+    print ("Leap year.")
 else:
     print ("Not leap year.")
