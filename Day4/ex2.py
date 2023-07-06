75c1b263aaa9c57b538999a2995649e5dbc14d82
DieSchoeneWolke <jan.schoenfeld@tutanota.de>
2023-07-06 12:18:20 +0200

    pseudocode

--- Day4/ex2.py
+++ Day4/ex2.py
@@ -2,3 +2,7 @@
 #Hint: Remember to import the random module here at the top of the file. 🎲
 	 
 #Write the rest of your code below this line 👇
+
+#import random function
+#define variable with random int 0 or 1
+#if else
