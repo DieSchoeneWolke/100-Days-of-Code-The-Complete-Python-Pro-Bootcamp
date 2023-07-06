21765583d48363a909f2197d23a618d0b8d8db89
DieSchoeneWolke <jan.schoenfeld@tutanota.de>
2023-07-06 10:34:38 +0200

    added conditionals and print

--- Day3/ex5.py
+++ Day3/ex5.py
@@ -26,3 +26,10 @@ e = lower_names.count("e")
 second_digit = l + o + v + e
 
 score = int(str(first_digit) + str(second_digit))
+
+if (score < 10) or (score > 90):
+  print(f"Your score is {score}, you go together like coke and mentos.")
+elif (score >= 40) and (score <= 50):
+  print(f"Your score is {score}, you are alright together.")
+else:
+  print(f"Your score is {score}.")
