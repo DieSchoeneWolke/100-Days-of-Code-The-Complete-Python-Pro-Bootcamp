817f8fb15200544ee373e9603000542dd6de5456
DieSchoeneWolke <jan.schoenfeld@tutanota.de>
2023-07-06 12:09:26 +0200

    added calc

--- Day4/ex1.py
+++ Day4/ex1.py
@@ -3,5 +3,7 @@ import random
 random_integer = random.randint(1, 10)
 print(random_integer)
 
-random_float = random.random()
+random_float = random.random() * 20
 print(random_float)
+
+# create a random decimal number between 0 and 20
